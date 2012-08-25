ad2eb29cf4871968bd9a0d99f526706bb1aad427
edent <terence.eden@o2.com>
2012-08-25 15:58:18 +0100

    Removed lists

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ require 'common/menu.php';
 require 'common/user.php';
 require 'common/theme.php';
 require 'common/twitter.php';
-require 'common/lists.php';
 require 'common/settings.php';
 
 require_once 'AppDotNet.php';
