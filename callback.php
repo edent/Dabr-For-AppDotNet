38a7c8af74033db855ef897f05162a792a1e45b5
edent <terence.eden@o2.com>
2012-09-19 11:30:48 +0100

    User is logged in with a cookie set until they log out

--- callback.php
+++ callback.php
@@ -5,16 +5,7 @@ require_once 'EZAppDotNet.php';
 $app = new EZAppDotNet();
 
 // log in user
-// if 'Remember me' was checked...
-if (isset($_SESSION['rem'])) {
-	// pass 1 into setSession in order
-	// to set a cookie and session
-	$token = $app->setSession(1);
-} else {
-
-	// otherwise just set session
-	$token = $app->setSession();
-}
+$token = $app->setSession(1);
 
 // redirect user after logging in
 header('Location: index.php');
